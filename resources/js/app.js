e47a72840d808aebae754ae19a42ace6a768e44f
bbyte47 <gregg@bari-flora.com>
2020-01-12 20:57:08 -0500

    6-Vue Component Registration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,17 @@
 require("./bootstrap");
 
-import router from "./routes";
 import VueRouter from "vue-router";
+import Index from "./Index";
+import router from "./routes";
 
 window.Vue = require("vue");
-
-Vue.component(
-    "example-component",
-    require("./components/ExampleComponent.vue").default
-);
-
-Vue.component("example-2", require("./components/Example2.vue").default);
 
 Vue.use(VueRouter);
 
 const app = new Vue({
     el: "#app",
-    router
+    router,
+    components: {
+        index: Index
+    }
 });
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,6 @@
-import ExampleComponent from "./components/ExampleComponent";
-import Example2 from "./components/Example2";
 import VueRouter from "vue-router";
+import Example2 from "./components/Example2";
+import ExampleComponent from "./components/ExampleComponent";
 
 const routes = [
     {
