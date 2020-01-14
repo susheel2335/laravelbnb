d5a891126f6574615e6008459de524f340a50a23
bbyte47 <gregg@bari-flora.com>
2020-01-14 13:15:59 -0500

    14-Props and props type

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <bookable-list-item></bookable-list-item>
-    <bookable-list-item></bookable-list-item>
-    <bookable-list-item></bookable-list-item>
-    <bookable-list-item></bookable-list-item>
+    <bookable-list-item
+      item-title="A cheap Villa"
+      item-content="A very cheap villa"
+      v-bind:price="1000"
+    ></bookable-list-item>
+    <bookable-list-item
+      item-title="A cheap Villa 2"
+      item-content="A very cheap villa 2"
+      :price="1500"
+    ></bookable-list-item>
   </div>
 </template>
 
