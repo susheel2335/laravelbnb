2c4f88fc6de364e7e9f4e486de5840140edf9d4e
bbyte47 <gregg@bari-flora.com>
2020-01-15 11:34:28 -0500

    17-Component state and reactivity

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <bookable-list-item
-      item-title="A cheap Villa"
-      item-content="A very cheap villa"
+      :item-title="bookable1.title"
+      :item-content="bookable1.content"
       v-bind:price="1000"
     ></bookable-list-item>
     <bookable-list-item
-      item-title="A cheap Villa 2"
-      item-content="A very cheap villa 2"
+      :item-title="bookable2.title"
+      :item-content="bookable2.content"
       :price="1500"
     ></bookable-list-item>
   </div>
@@ -20,11 +20,30 @@ export default {
   components: {
     BookableListItem
   },
+  data() {
+    return {
+      bookable1: {
+        title: "Cheap Villa!",
+        content: "A very cheap villa"
+      },
+      bookable2: {
+        title: "Cheap Villa 2",
+        content: "A very cheap villa 2"
+      }
+    };
+  },
   //   beforeCreate() {
   //     console.log("before create");
   //   },
   created() {
     console.log("created");
+    console.log(this.bookable1);
+    console.log(this.bookable2);
+
+    setTimeout(() => {
+      this.bookable1.title = "Expensive Villa";
+      this.bookable2.title = "Very Expensive Villa";
+    }, 5000);
   }
   //   beforeMount() {
   //     console.log("before mount");
